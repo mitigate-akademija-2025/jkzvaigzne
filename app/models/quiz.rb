830cd950089798ba6943b10a02abfaaa60393f4a
Jēkabs Andrejs Zvaigzne <47506475+jkzvaigzne@users.noreply.github.com>
2025-08-07 13:22:24 +0000

    Quiz update - adding table, added new layout

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,4 +3,8 @@ class Quiz < ApplicationRecord
     has_many :questions, dependent: :destroy
 
     accepts_nested_attributes_for :questions, allow_destroy: true
+
+    def to_s 
+        return title
+    end
 end
